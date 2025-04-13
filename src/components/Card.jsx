0b2adb28c3e010ec1b8dd0890261b385886a1aef
bleverbd <bleverbd@gmail.com>
2025-04-13 18:12:18 +0600

    last

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,77 @@
-import React from 'react'
+import React from "react";
 
-function Card() {
+
+function Card({card_bd,card_profile,card_star,card_icon,btn_icon,}) {
   return (
-    <div>
-        <p> This is Card Section</p>
-      
+    <div className="font-Syne w-[332px] group">
+      <div className=" group overflow-hidden rounded-xl relative duration-300 group-hover:shadow-2xl group-hover:-translate-y-3">
+
+
+        <div className="absolute top-3 left-5 z-20">
+          <button className="bg-[#B45C3D] text-white text-xs rounded-sm text-center px-3 py-2">
+            10% Discount
+          </button>
+        </div>
+
+
+
+        <div>
+          <img
+            className="w-[40px] h-[40px] bg-white rounded-full p-[10px] absolute top-3 right-5 z-20"
+            src={card_icon}
+            alt="card_icon"
+          />
+        </div>
+
+        <div className="  overflow-hidden ">
+          <img
+            className="w-[332px] h-[290px] object-cover rounded-t-xl group-hover:scale-110 duration-300"
+            src={card_bd}
+            alt="card bd"
+          />
+        </div>
+
+        <div className=" flex flex-col gap-10 my-5 mx-5">
+          <div className="flex items-center gap-20 w-[332px]">
+            <div className="flex gap-2 items-center">
+              <img
+                className="rounded-full w-[40px] h-[40px]"
+                src={card_profile}
+                alt="card_profile"
+              />
+              <p className="text-[#6A7283] font-medium"> John Doe </p>
+            </div>
+
+            <div className="flex gap-2 items-center">
+              <img src={card_star} alt="card_star" />
+              <p> 4.5 (158) </p>
+            </div>
+          </div>
+          <div>
+            <p className="text-[#192A48] font-semibold text-xl">
+              JA Solar 200W
+            </p>
+          </div>
+          <div className="flex items-center  gap-13 w-[332px]">
+            <div>
+              <p className="text-[#6A7283] text-sm">Tax excluded</p>
+              <p className="text-[#071431] font-semibold text-xl"> 55,00 € </p>
+            </div>
+            <div className="cursor-pointer outline rounded-4xl w-40 pl-5 py-3 group-hover:bg-bg-btn-color group-hover:outline-none">
+              <button className="flex gap-2  items-center cursor-pointer ">
+                <p className="text-[#071431] font-medium">View Details</p>
+                <img
+                  className="group-hover:translate-x-2 duration-300"
+                  src={btn_icon}
+                  alt="btn_icon"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- src/pages/marketplace/Marketplace.jsx
+++ src/pages/marketplace/Marketplace.jsx
@@ -3,11 +3,58 @@ import Hero from './Hero'
 import Card from '../../components/Card'
 
 
-const Marketplace = () => {
+import card_profile1 from "../../assets/card_profile1.png";
+import card_bd1 from "../../assets/card_bd_1.png";
+import card_profile2 from "../../assets/card_profile2.png";
+import card_bd2 from "../../assets/card_bd_2.png";
+import card_profile3 from "../../assets/card_profile3.png";
+import card_bd3 from "../../assets/card_bd_3.png";
+import card_profile4 from "../../assets/card_profile1.png";
+import card_bd4 from "../../assets/card_bd_4.png";
+import card_star1 from "../../assets/card_icon2.svg";
+import card_icon1 from "../../assets/card_icon1.svg";
+import btn_icon1 from "../../assets/btn_icon.svg";
+
+import search_icon from "../../assets/search_icon.svg"
+
+
+
+
+const Marketplace = (card_star,card_icon,btn_icon) => {
   return (
-    <div>
+    <div class="font-Syne">
       <Hero/>
-      <Card/>
+      <br/>
+
+      <div className="max-w-[1520px] mx-auto my-20">
+      <div class="flex items-center justify-between">
+        <div className="text-[#071431] text-5xl font-semibold"> <p>Your market for solar panels</p>  </div>
+
+        <div className=" relative"> 
+          <div className="bg-[#F5F6F7] rounded-3xl px-10 py-3 outline-1 outline-[#d6d8da] w-100" > <input className="focus:outline-none" type="search" placeholder="Search for product" /></div>
+          <div className="absolute top-3 right-5"> <img src={search_icon}/> </div>
+        </div>
+      </div>
+
+       <div className="grid grid-cols-3 mt-50">
+
+       <Card card_bd={card_bd1} card_profile={card_profile1} card_star={card_star1} card_icon={card_icon1} btn_icon={btn_icon1} />
+
+       <Card card_bd={card_bd2} card_profile={card_profile2} card_star={card_star1} card_icon={card_icon1} btn_icon={btn_icon1} />
+
+       <Card card_bd={card_bd3} card_profile={card_profile3} card_star={card_star1} card_icon={card_icon1} btn_icon={btn_icon1} />
+
+       <Card card_bd={card_bd4} card_profile={card_profile1} card_star={card_star1} card_icon={card_icon1} btn_icon={btn_icon1} />
+
+       <Card card_bd={card_bd1} card_profile={card_profile1} card_star={card_star1} card_icon={card_icon1} btn_icon={btn_icon1} />
+      <Card card_bd={card_bd3} card_profile={card_profile3} card_star={card_star1} card_icon={card_icon1} btn_icon={btn_icon1} />
+
+      <Card card_bd={card_bd4} card_profile={card_profile1} card_star={card_star1} card_icon={card_icon1} btn_icon={btn_icon1} />
+
+      <Card card_bd={card_bd2} card_profile={card_profile2} card_star={card_star1} card_icon={card_icon1} btn_icon={btn_icon1} />
+     
+       </div>
+      </div>
     </div>
   )
 }
