e85a5a0aa9e572d6b6161c37f58c590af46d290f
bleverbd <bleverbd@gmail.com>
2025-04-19 10:38:08 +0600

    last

--- src/components/InIcon.jsx
+++ src/components/InIcon.jsx
@@ -5,10 +5,14 @@ import decIcon from "../assets/dec_icon.svg";
 
 const InIcon = () => {
   return (
-    <div>
+    <div className="font-Syne"> 
       <div>
-       <div className="w-[48px] h-[48px] bg-[#071431] relative"> <img className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" src={decIcon}/></div>
-        <div><img src={incIcon}/></div>
+       <div className="w-[32px] h-[32px] bg-[#071431] relative rounded-full"> <img className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" src={decIcon}/></div>
+       <div className="rounded-full outline-2 outline-[#071431]">
+        <p className="text-[#071431]"> 01 </p>
+       </div>
+       <div className="w-[32px] h-[32px] bg-[#071431] relative rounded-full"> <img className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" src={incIcon}/></div>
+
       </div>
     </div>
   )
